be6fb596a5276afad716c11059173fd3ac36ff70
tailtq <tai.ltq.learning@gmail.com>
2020-03-25 22:14:17 +0700

    Update

---
+++ kaggle/dogs-vs-cats/split-images.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+
+from os.path import isfile, join
+
+os.chdir('train')
+
+os.mkdir('cat')
+os.mkdir('dog')
+
+files = [f for f in os.listdir() if isfile(join(f))]
+
+for file in files:
+    if file[0:3] == 'cat':
+        shutil.move(file, 'cat')
+    else:
+        shutil.move(file, 'dog')
